bf3918070f96f668ea06c0a2b4f046fef1a4b9f5
yrlp98 <yousef.r1999@gmail.com>
2021-11-09 12:51:16 +0330

    Initial commit

---
+++ pages/about.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>About DadJoks</h1>
+    <p>This is an app that displays conry dad jokes</p>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "About DadJoks",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best place for corny dad jokes",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
